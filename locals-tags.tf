8a38bbaa68655bbe51cfcc00c7cf953d933773e4
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-03-13 09:17:33 +0100

    Fixing incorrect variable names

--- locals-tags.tf
+++ locals-tags.tf
@@ -2,13 +2,13 @@ locals {
   default_tags = {
     Environment    = var.environment
     Application    = var.application
-    CostCenter     = var.costcenter
+    CostCenter     = var.cost_center
     Change         = var.change
     Owner          = var.owner
     SPOC           = var.spoc
-    TLP            = var.tlp
+    TLP            = var.tlp_colour
     CiaRating      = var.cia_rating
-    TechnicalOwner = var.technicalowner
+    TechnicalOwner = var.technical_owner
 
   }
 }
